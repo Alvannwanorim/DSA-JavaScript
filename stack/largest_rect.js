546dd92ee866292188220a31f881de2643950b61
alvannwanorim <alvannwanorim@gmail.com>
2022-08-16 08:17:39 +0100

    Feat: Largest Reactangle in a Histogram - Stack

--- stack/largest_rect.js
+++ stack/largest_rect.js
@@ -7,6 +7,7 @@ function largetRectInHisto(heights) {
 		while (stack.length && stack[stack.length - 1][1] > heights[i]) {
 			const [ index, height ] = stack.pop();
 			maxArea = Math.max(maxArea, height * (i - index));
+			newIndex = index;
 		}
 		stack.push([ newIndex, heights[i] ]);
 	}
@@ -14,10 +15,11 @@ function largetRectInHisto(heights) {
 	console.log(maxArea, stack);
 	for (let j = 0; j < stack.length; j++) {
 		let currArea = stack[j][1] * (heights.length - stack[j][0]);
+
 		maxArea = Math.max(maxArea, currArea);
 	}
 
 	return maxArea;
 }
 
-console.log(largetRectInHisto([ 1, 2, 5, 3, 1 ]));
+console.log(largetRectInHisto([ 1, 3, 4, 2, 1 ]));
