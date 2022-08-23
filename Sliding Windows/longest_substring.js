02eda525d2cd12be0052f3f5a771e36d17b88ad6
alvannwanorim <alvannwanorim@gmail.com>
2022-08-23 08:40:09 +0100

    Feat: Longest SubString - Sliding Windows

---
+++ Sliding Windows/longest_substring.js
@@ -0,0 +1,16 @@
+function longestSubSring(s) {
+	let l = 0;
+	let max = 0;
+	let result = new Set();
+	for (let i = 0; i < s.length; i++) {
+		while (result.has(s[i])) {
+			result.delete(s[l]);
+			l++;
+		}
+		result.add(s[i]);
+		max = Math.max(max, i - l + 1);
+	}
+	return max;
+}
+
+console.log(longestSubSring('abacabcabcd'));
