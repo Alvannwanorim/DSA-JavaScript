720fa7583e0de1ebf2d7d26e1a2e68972aecda2c
alvannwanorim <alvannwanorim@gmail.com>
2022-08-30 23:49:59 +0100

    Reverse a Linked List - Linked List

---
+++ Linked List/reverse_linked_list.js
@@ -0,0 +1,13 @@
+class Node {
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+	}
+}
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+}
