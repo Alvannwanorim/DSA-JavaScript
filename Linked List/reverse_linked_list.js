5ebb645474fc415892ebc5f1fa8efe2b2ac88f1a
alvannwanorim <alvannwanorim@gmail.com>
2022-08-30 23:56:48 +0100

    Reverse a Linked List - Linked List

--- Linked List/reverse_linked_list.js
+++ Linked List/reverse_linked_list.js
@@ -10,4 +10,23 @@ class LinkedList {
 		this.tail = null;
 		this.size = 0;
 	}
+	push(val) {
+		const node = new Node(val);
+		if (this.size === 0) {
+			this.head = node;
+			this.tail = node;
+		} else {
+			const oldtail = this.tail;
+			this.tail = node;
+			node.next = this.tail;
+		}
+		this.size += 1;
+		return this;
+	}
 }
+
+let sll = new LinkedList();
+sll.push(1);
+sll.push(2);
+sll.push(3);
+console.log(sll);
