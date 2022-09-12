b8b6df2c7d3260c6fb164fcbc98d6f9aff1a4eaa
alvannwanorim <alvannwanorim@gmail.com>
2022-09-12 21:31:16 +0100

    Feat: CanSum - Dynamic Programming

--- Dynamic Programming/can_sum.js
+++ Dynamic Programming/can_sum.js
@@ -1,11 +1,16 @@
-function canSum(target, numbers) {
+function canSum(target, numbers, memo = {}) {
+	if (target in memo) return memo[target];
 	if (target === 0) return true;
 	if (target < 0) return false;
 	for (let num of numbers) {
 		let diff = target - num;
-		if (canSum(diff, numbers) === true) return true;
+		if (canSum(diff, numbers, memo) === true) {
+			memo[target] = true;
+			return true;
+		}
 	}
+	memo[target] = false;
 	return false;
 }
 
-console.log(canSum(1, [ 2, 3, 5, 6, 7, 8 ]));
+console.log(canSum(300, [ 7, 14 ]));
