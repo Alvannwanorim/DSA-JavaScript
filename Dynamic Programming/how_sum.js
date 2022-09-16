69574dcbd74a1c61df39bbcedfc0f6d97aa6f913
alvancode <alvan.nwanorim@babbangona.com>
2022-09-16 16:51:51 +0100

    Find array of poosible sums of target - Dynamic programming

--- Dynamic Programming/how_sum.js
+++ Dynamic Programming/how_sum.js
@@ -1,10 +1,11 @@
 /**
  * TC: O(n^m*m) (brute force)  n= nums.length, m = target
- * TC: O(n*m*m) (Optimized)
- * SC: O(m) n= nums.length   
+ * SC: O(m) n= nums.length
+ * TC: O(n*m^2) (Optimized)
+ * SC: O(m^2) (optimized) n= nums.length   
  * @param target: number
  * @param nums: number[]
- * @returns []
+ * @returns [] 
  */
 function howSum(target, nums, memo = {}) {
 	if (target in memo) return memo[target];
