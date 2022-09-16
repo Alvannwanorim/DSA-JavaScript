7afe3979c27c9e1dc974ae14956f06a13d5788cb
alvannwanorim <alvannwanorim@gmail.com>
2022-09-16 21:55:43 +0100

    Find array of poosible sums of target - Dynamic programming

---
+++ Dynamic Programming/best_sum.js
@@ -0,0 +1,26 @@
+/**
+ * TC: O(n^m*m) (brute force)  n= nums.length, m = target
+ * SC: O(m) n= nums.length
+ * TC: O(n*m^2) (Optimized)
+ * SC: O(m^2) (optimized) n= nums.length   
+ * @param target: number
+ * @param nums: number[]
+ * @returns [] 
+ */
+
+function bestSum(target, numbers) {
+	if (target === 0) return [];
+	if (target < 0) return null;
+
+	let shortestCombination = null;
+	for (let number of numbers) {
+		let diff = target - number;
+		const remainder = bestSum(diff, numbers);
+		if (remainder !== null) {
+			const combination = [ ...remainder, number ];
+			if ((shortestCombination === null || shortestCombination, length > combination.length)) {
+				shortestCombination = combination;
+			}
+		}
+	}
+}
