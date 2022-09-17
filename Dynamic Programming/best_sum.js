2d464fe7f25f05ae8973804bc4b9cac46c29c6ab
alvannwanorim <alvannwanorim@gmail.com>
2022-09-17 09:38:38 +0100

    Find array of poosible sums of target - Dynamic programming

--- Dynamic Programming/best_sum.js
+++ Dynamic Programming/best_sum.js
@@ -1,21 +1,24 @@
 /**
- * TC: O(n^m*m) (brute force)  n= nums.length, m = target
- * SC: O(m) n= nums.length
- * TC: O(n*m^2) (Optimized)
- * SC: O(m^2) (optimized) n= nums.length   
+ * brute force
+ * TC: O(n^m * m)  n= nums.length, m = target
+ * SC: O(m^2) n= nums.length, m = target
+ * Optimized
+ * TC: O(n*m^2) n= nums.length, m = target
+ * SC: O(m^2) n= nums.length, m = target 
  * @param target: number
  * @param nums: number[]
  * @returns [] 
  */
 
-function bestSum(target, numbers) {
+function bestSum(target, numbers, memo = {}) {
+	if (target in memo) return memo[target];
 	if (target === 0) return [];
 	if (target < 0) return null;
 
 	let shortestCombination = null;
 	for (let number of numbers) {
 		let diff = target - number;
-		const remainder = bestSum(diff, numbers);
+		const remainder = bestSum(diff, numbers, memo);
 		if (remainder !== null) {
 			const combination = [ ...remainder, number ];
 			if (shortestCombination === null || shortestCombination.length > combination.length) {
@@ -23,8 +26,9 @@ function bestSum(target, numbers) {
 			}
 		}
 	}
-
+	memo[target] = shortestCombination;
 	return shortestCombination;
 }
 
 console.log(bestSum(7, [ 5, 3, 4 ]));
+console.log(bestSum(100, [ 1, 2, 5, 25 ]));
