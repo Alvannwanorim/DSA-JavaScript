c0d1af590a834fc83656d4559034ebd01ab0a1aa
alvannwanorim <alvannwanorim@gmail.com>
2022-09-17 12:44:12 +0100

    Generate target string from array of strings(wordbank) - Dynamic programming

--- Dynamic Programming/can_construct.js
+++ Dynamic Programming/can_construct.js
@@ -1,11 +1,28 @@
-function canConstruct(target, wordBank) {
+/**
+ * brute force
+ * TC: O(n^m * m)  n= wordBank.length, m = target.length
+ * SC: O(m^2) n= wordBank.length, m = target.length
+ * Optimized
+ * TC: O(n*m^2) n= n= wordBank.length, m = target.length
+ * SC: O(m^2) n= n= wordBank.length, m = target.length 
+ * @param target: string
+ * @param wordBank: string[]
+ * @returns [] 
+ */
+
+function canConstruct(target, wordBank, memo = {}) {
+	if (target in memo) return memo[target];
 	if (target === '') return true;
 	for (let word of wordBank) {
 		if (target.indexOf(word) === 0) {
 			const suffix = target.slice(word.length);
-			if (canConstruct(suffix, wordBank) === true) return true;
+			if (canConstruct(suffix, wordBank, memo) === true) {
+				memo[target] = true;
+				return true;
+			}
 		}
 	}
+	memo[target] = false;
 	return false;
 }
 
