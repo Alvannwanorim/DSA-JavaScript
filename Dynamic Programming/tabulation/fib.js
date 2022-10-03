346394fb9028f3853c4a1364e24c9324dc5c16ed
alvannwanorim <alvannwanorim@gmail.com>
2022-10-03 10:30:40 +0100

    Fibonacci Tabulation method - Dynamic Programming

---
+++ Dynamic Programming/tabulation/fib.js
@@ -0,0 +1,16 @@
+function fib(n) {
+	const table = Array(n + 1).fill(0);
+	table[1] = 1;
+	for (let i = 0; i <= n; i++) {
+		table[i + 1] += table[i];
+		table[i + 2] += table[i];
+	}
+
+	console.log(table);
+	return table[n];
+}
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
