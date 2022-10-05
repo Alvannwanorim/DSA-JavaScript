ece2644fcca9c9df2729fb82a8fe792c74ead5de
alvannwanorim <alvannwanorim@gmail.com>
2022-10-05 19:18:05 +0100

    can-construct  Tabulation method - Dynamic Programming

--- Dynamic Programming/tabulation/can_construct.js
+++ Dynamic Programming/tabulation/can_construct.js
@@ -1,8 +1,20 @@
-function canConstruct(target, wordBank) {}
+function canConstruct(target, wordBank) {
+	const table = Array(target.length + 1).fill(false);
+	table[0] = true;
+
+	for (let i = 0; i < target.length; i++) {
+		if (table[i] === true) {
+			for (let word of wordBank) {
+				if (target.slice(i, i + word.length) === word) table[i + word.length] = true;
+			}
+		}
+	}
+	return table[target.length];
+}
 
 console.log(canConstruct('abcdef', [ 'ab', 'abc', 'cd', 'def', 'abcd' ]));
 
 console.log(canConstruct('skateboard', [ 'bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar' ]));
 console.log(
-	canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [ 'e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee', 'eeeeeee' ])
+	canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', [ 'e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee', 'eeeeeee' ])
 );
