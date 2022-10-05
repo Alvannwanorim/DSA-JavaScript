b597b63194748b055a6355fa280cdd31a1f137eb
alvannwanorim <alvannwanorim@gmail.com>
2022-10-05 19:47:16 +0100

    all-construct  Tabulation method - Dynamic Programming

---
+++ Dynamic Programming/tabulation/all_construct.js
@@ -0,0 +1,20 @@
+function allConstruct(target, wordBank) {
+	const table = Array(target.length + 1).fill().map(() => []);
+	table[0] = [ [] ];
+
+	for (let i = 0; i < target.length; i++) {
+		for (let word of wordBank) {
+			if (target.slice(i, i + word.length) === word) {
+				const newCombo = table[i].map((sub) => [ ...sub, word ]);
+				// console.log(newCombo);
+				table[i + word.length].push(...newCombo);
+			}
+		}
+	}
+
+	return table[target.length];
+}
+
+console.log(allConstruct('abcdef', [ 'ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c' ]));
+// console.log(allConstruct('skateboard', [ 'bo', 'rd', 'ate', 't', 'ska', 'sk', 'boa' ]));
+// console.log(allConstruct('eeeeeeeeeeeeee', [ 'e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee', 'eeeeeee' ]));
